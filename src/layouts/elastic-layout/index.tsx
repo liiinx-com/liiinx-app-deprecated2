e5410d24ef0ada5d44079a65ebab923456cbc27c
cryptoteq <cryptoteq.ai@gmail.com>
2023-05-20 14:37:40 -0400

    feat: add website api route

---
+++ src/layouts/elastic-layout/index.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactNode } from "react";
+
+interface ElasticLayoutProps {
+  children: ReactNode;
+  // app: App;
+  // config: LayoutConfig;
+}
+
+const ElasticLayout: FC<ElasticLayoutProps> = () => {
+  return <div>ElasticLayout</div>;
+};
+
+export default ElasticLayout;
